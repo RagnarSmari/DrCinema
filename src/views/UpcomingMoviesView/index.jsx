08c10e3aae09a5351eb73ebf34d02437f3894ff6
agustt20 <70704919+agustt20@users.noreply.github.com>
2021-12-07 15:04:10 +0000

    It works on my computer

--- src/views/UpcomingMoviesView/index.jsx
+++ src/views/UpcomingMoviesView/index.jsx
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { FlatList, Text, View } from 'react-native';
+import {
+  FlatList, Image, Text, TouchableOpacity, View,
+} from 'react-native';
+import styles from '../../styles';
 
 const UpcomingMoviesView = function () {
   const movies = useSelector((s) => s.upcomingMovies);
-  console.log(movies);
+  const renderMovie = ({ item }) => (
+    <View style={[styles.card, styles.shadowProp]}>
+      <TouchableOpacity onPress={console.log(item)}>
+        <View style={styles.imgContainer}>
+          <Image style={styles.moviePoster} source={{ uri: item.poster }} />
+        </View>
+      </TouchableOpacity>
 
-  const renderMovie = ({ item }) => {
-    console.log(item);
-    const test = 'test';
-    return (
-      <View>
-        <Text>{item.title}</Text>
-      </View>
-    );
-  };
+      <Text style={styles.text}>
+        {item.title}
+      </Text>
+      <Text style={styles.text}>
+        {item['release-dateIS']}
+      </Text>
+
+    </View>
+  );
 
   return (
-    <View>
-      <FlatList data={movies} renderItem={renderMovie} />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={movies}
+        renderItem={renderMovie}
+        numColumns={1}
+        nestedScrollEnabled
+        vertical
+        keyExtractor={(s) => s.id}
+      />
     </View>
   );
 };
