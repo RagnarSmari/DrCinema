805c2ef38f2375685578456e80913c85662b6647
agustt20 <70704919+agustt20@users.noreply.github.com>
2021-12-07 10:07:30 +0000

    CinemaDetailView Hálfklárað

--- src/views/CinemaDetailView/index.jsx
+++ src/views/CinemaDetailView/index.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import {
-  View, Text, FlatList, Image,
+  View, Text, FlatList, Image, TouchableOpacity,
 } from 'react-native';
 import { useSelector } from 'react-redux';
 import movieService from '../../services/movieService';
+import styles from '../../styles';
 
 const CinemaDetailView = function (props) {
   const { cinema } = props.route.params;
   const [movies, setMovies] = useState([]);
   const allMovies = useSelector((s) => s.movies);
+  console.log(props);
 
   useEffect(async () => {
     const myMovies = movieService.getMoviesByCinemaId(cinema.id, allMovies);
@@ -16,21 +18,27 @@ const CinemaDetailView = function (props) {
   }, []);
 
   const renderMovie = ({ item }) => (
-    <View>
-      <Image source={{
-        uri: item.poster.toString(),
-      }}
-      />
+    <View style={[styles.card, styles.shadowProp]}>
       <Text>
         {item.title}
       </Text>
-      <Text />
+      <TouchableOpacity onPress={() => props.navigation.navigate('MovieDetailView')}>
+        <Image style={{ width: '70%', height: '70%' }} source={{ uri: item.poster }} />
+      </TouchableOpacity>
+
     </View>
   );
 
   return (
     <View>
-      <FlatList data={movies} renderItem={renderMovie} keyExtractor={(s) => s.id} />
+      <FlatList
+        data={movies}
+        renderItem={renderMovie}
+        keyExtractor={(s) => s.id}
+        numColumns={1}
+        nestedScrollEnabled
+        vertical
+      />
     </View>
   );
 };
